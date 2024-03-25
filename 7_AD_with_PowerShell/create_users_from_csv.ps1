cf9c9a151de40089fd54102871e589c5043d5daf
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-03-25 21:52:12 +0530

    Update create_users_from_csv.ps1

--- 7_AD_with_PowerShell/create_users_from_csv.ps1
+++ 7_AD_with_PowerShell/create_users_from_csv.ps1
@@ -2,7 +2,7 @@
 Import-Module ActiveDirectory
 
 # Path of the csv File with User Details
-$filePath       = "C:\Users\azka.aslam\Desktop\users-block - Sheet1.csv"
+$filePath       = "C:\Users\azka.aslam\Desktop\users-list.csv"
 
 # Import CSV as an Array
 $usersAll       = Import-Csv $filePath
@@ -14,20 +14,27 @@ $password       =  Read-Host -Prompt "Enter a Strong Password"
 ForEach ($user in $usersAll) {
 
 # Variables
+$userName       =  ($user.'First Name' + " " + $user.'Last Name')
 $emailAddress   =  $user.'Email Address'
 $logonName      =  "$emailAddress".Replace("@gmail.com", "").ToLower()
- 
-# Calling each User
-New-ADUser `
-    -Name ($user.'First Name' + " " + $user.'Last Name') `
-    -GivenName $user.'First Name' `
-    -Surname $user.'Last Name' `
-    -UserPrincipalName "$logonName@azka.local" `
-    -AccountPassword (ConvertTo-SecureString "$password" -AsPlainText -Force) `
-    -Path $user.'Organizational Unit' `
-    -DisplayName $user.'Display Name' `
-    -SamAccountName "$logonName" `
-    -EmailAddress $user.'Email Address' `
-    -Enabled ([System.Convert]::ToBoolean($user.Enabled))`
-    -ChangePasswordAtLogon 1
+
+if (Get-ADUser -F {Name -eq $userName}) {
+    Write-Warning "Username already exists. Please choose a different Name."
+ }
+else {
+    New-ADUser `
+      -Name "$userName" `
+      -GivenName $user.'First Name' `
+      -Surname $user.'Last Name' `
+      -UserPrincipalName "$logonName@azka.local" `
+      -AccountPassword (ConvertTo-SecureString "$password" -AsPlainText -Force) `
+      -Path $user.'Organizational Unit' `
+      -DisplayName $user.'Display Name' `
+      -SamAccountName "$logonName" `
+      -EmailAddress $user.'Email Address' `
+      -Enabled ([System.Convert]::ToBoolean($user.Enabled))`
+      -Title $user.'Job Title' `
+      -ChangePasswordAtLogon 1
+    }
 }
+ 
